eda063a47114456d28dcab45f4b54bac8794ffd6
Your Name <gogemaza14@gmail.com>
2024-10-08 20:01:39 +0400

    Functional Update

---
+++ src/app/common/props.ts
@@ -0,0 +1,6 @@
+export interface PrintDataProps {
+  questionId: string;
+  question: string;
+  correctAnswer: string;
+  answers: string[];
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,16 @@
 import { useEffect, useState } from "react";
 import questionData from "./data/questionData.json";
 import { shuffleArray } from "./helpers/helpers";
-
-interface PrintDataProps {
-  quetionId: string;
-  question: string;
-  answers: string[];
-}
+import { PrintDataProps } from "./common/props";
 
 const Home = () => {
-  const [category, setCategory] = useState<string>();
-  const [questionIndex, setQuestionIndex] = useState<number>(4);
+  const [category, setCategory] = useState<string>("");
+  const [questionIndex, setQuestionIndex] = useState<number>(1000);
   const [printData, setPrintData] = useState<PrintDataProps[]>([]);
   const [chosenAnswer, setChosenAnswer] = useState<string>("");
   const [isAnswerSubmitted, setIsAnswerSubmitted] = useState<boolean>(false);
+  const [score, setScore] = useState<number>(0);
+  console.log(questionIndex, printData.length);
 
   useEffect(() => {
     if (category) {
@@ -26,29 +23,77 @@ const Home = () => {
     }
   }, [category]);
 
+  useEffect(() => {
+    if (isAnswerSubmitted) {
+      const correctAnswer = printData[questionIndex].correctAnswer;
+      if (correctAnswer === chosenAnswer) {
+        setScore(score + 1);
+      }
+    }
+  }, [isAnswerSubmitted]);
+
   return (
     <>
-      {questionIndex === 4 && (
+      {questionIndex === 1000 && (
         <div className="w-[100vw] h-[100vh] flex justify-center items-center gap-4">
           <p>choose category</p>
+          <button
+            onClick={() => {
+              setCategory("HTML");
+              setQuestionIndex(0);
+            }}
+          >
+            HTML
+          </button>
+          <button
+            onClick={() => {
+              setCategory("CSS");
+              setQuestionIndex(0);
+            }}
+          >
+            CSS
+          </button>
           <button
             onClick={() => {
               setCategory("javascript");
               setQuestionIndex(0);
             }}
           >
-            javascript
+            Javascript
           </button>
-          <button>typescript</button>
-          <button>module</button>
-          <button>fuckboy</button>
+          <button
+            onClick={() => {
+              setCategory("accessibility");
+              setQuestionIndex(0);
+            }}
+          >
+            Accessibility
+          </button>
         </div>
       )}
-      {questionIndex !== 4 && printData.length > 0 && questionIndex < printData.length && (
+      {questionIndex !== 1000 && printData.length > 0 && questionIndex < printData.length && (
         <div className="w-[100vw] h-[100vh] flex justify-center items-center gap-4">
+          <h1>
+            {questionIndex + 1}/{printData.length}
+          </h1>
           <div>{printData[questionIndex].question}</div>
-          {printData[questionIndex].answers.map((multipleChoice) => {
-            return <button onClick={() => setChosenAnswer(multipleChoice)}>{multipleChoice}</button>;
+          {printData[questionIndex].answers.map((multipleChoice, index) => {
+            const correctAnswer = printData[questionIndex].correctAnswer;
+            return (
+              <button
+                key={index}
+                className={` ${isAnswerSubmitted && correctAnswer === multipleChoice ? "bg-[green]" : ""} ${
+                  multipleChoice === chosenAnswer && !isAnswerSubmitted ? "bg-[yellow]" : ""
+                } ${multipleChoice === chosenAnswer && isAnswerSubmitted && chosenAnswer !== correctAnswer ? "bg-[red]" : ""} `}
+                onClick={() => {
+                  if (!isAnswerSubmitted) {
+                    setChosenAnswer(multipleChoice);
+                  }
+                }}
+              >
+                {multipleChoice}
+              </button>
+            );
           })}
           {!isAnswerSubmitted && (
             <button
@@ -64,12 +109,17 @@ const Home = () => {
           {isAnswerSubmitted && (
             <button
               onClick={() => {
-                setIsAnswerSubmitted(false);
                 setQuestionIndex(questionIndex + 1);
+                setIsAnswerSubmitted(false);
               }}
             >
               next question
             </button>
+          )}
+          {questionIndex >= printData.length && (
+            <div className="w-[100vw] h-[100vh] flex justify-center items-center gap-4">
+              your score is {score} out of {printData.length}
+            </div>
           )}
         </div>
       )}
